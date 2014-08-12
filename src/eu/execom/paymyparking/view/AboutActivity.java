bfb20a83b1be795a35d1a119d577583e9cfbaa71
Branislav <branislav.rus@gmail.com>
2014-08-12 20:32:48 +0200

    Navigation implemented.

--- src/eu/execom/paymyparking/view/AboutActivity.java
+++ src/eu/execom/paymyparking/view/AboutActivity.java
@@ -2,7 +2,6 @@ package eu.execom.paymyparking.view;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import eu.execom.paymyparking.R;
 
 public class AboutActivity extends Activity {
@@ -12,12 +11,4 @@ public class AboutActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.about, menu);
-		return true;
-	}
-
 }
--- src/eu/execom/paymyparking/view/SettingsActivity.java
+++ src/eu/execom/paymyparking/view/SettingsActivity.java
@@ -3,15 +3,10 @@ package eu.execom.paymyparking.view;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -33,8 +28,6 @@ public class SettingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-		// Show the Up button in the action bar.
-		setupActionBar();
 		updateLicensePlates();
 		addListeners();
 		updateNewEnabled();
@@ -125,41 +118,10 @@ public class SettingsActivity extends Activity {
 		((EditText) findViewById(R.id.newLicensePlate)).setText("");
 		ViewHelper.getApplication(this).getData().getLicensePlates().add(licensePlate);
 		SharedPreferencesService.SaveLicensePlates(this);
+		if (ViewHelper.getApplication(this).getData().getLicensePlates().size() == 1) {
+			ViewHelper.getApplication(this).getData().setDefaultLicensePlate(ViewHelper.getApplication(this).getData().getLicensePlates().get(0));
+			SharedPreferencesService.SaveDefaultLicensePlate(this);
+		}
 		adapter.notifyDataSetChanged();
 	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.settings, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }
